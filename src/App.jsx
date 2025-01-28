b32ae9cec531accf0ebfe91317e2f0959d2faffe
Nemanja Stojanovic <feemail042@gmail.com>
2025-01-28 19:30:04 +0100

    fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchComponent from "./components/SearchComponent";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
+
+  const fetchMovies = async () => {
+    try {
+      const res = await fetch(API_BASE_URL, API_OPTIONS);
+      console.log(res, "res");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <main>
       <div className="pattern">
